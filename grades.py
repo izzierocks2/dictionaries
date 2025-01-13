720aaa849c5134cd088cf437efa826e8db6c620e
Grace <gracek2705@tuckahoeschools.org>
2025-01-13 13:32:09 +0000

    commit

---
+++ grades.py
@@ -0,0 +1,7 @@
+names = ["Maya", "JP", "Erica", "Vincent"] #defining a list
+grades = [100, 99, 98, 80]
+
+print(f"{names[0]} grade is {grades[0]}")
+
+for i in range(len(names)):
+    print(f"{names[i]} grade is {grades[i]}")
